60fc09d5bd513cb4f9121b4039709a073fa2149f
rokuosan <85651386+rokuosan@users.noreply.github.com>
2023-03-17 01:03:36 +0900

    イベントをリスナに登録した

--- youtube_archiver/youtube_archiver_app.py
+++ youtube_archiver/youtube_archiver_app.py
@@ -21,8 +21,9 @@ class YouTubeArchiver(ft.UserControl):
     def build(self):
         self.video_list = ft.ListView(expand=1, spacing=0, padding=0, auto_scroll=True)
 
-        self.download_url = ft.TextField(hint_text='YouTube URL', expand=True)
-        self.download_btn = ft.IconButton(ft.icons.DOWNLOAD)
+        self.download_url: ft.TextField = ft.TextField(hint_text='YouTube URL',
+                                                       expand=True, on_submit=self.download_clicked)
+        self.download_btn: ft.IconButton = ft.IconButton(ft.icons.DOWNLOAD, on_click=self.download_clicked)
 
         return ft.Column(
             width=400,
@@ -38,3 +39,21 @@ class YouTubeArchiver(ft.UserControl):
                 self.video_list
             ]
         )
+
+    def download_clicked(self, e):
+        url: str = self.download_url.value
+
+        if url.replace(' ', '') == '':
+            self.page.snack_bar = ft.SnackBar(ft.Text('URL is empty!'))
+            self.page.snack_bar.open = True
+            self.page.update()
+            return
+        elif not url.startswith('https://www.youtube.com/watch?v=') and not url.startswith('https://youtu.be/'):
+            self.page.snack_bar = ft.SnackBar(ft.Text('URL is not youtube!'))
+            self.page.snack_bar.open = True
+            self.page.update()
+            return
+
+        self.download_url.value = ''
+        self.page.update()
+
