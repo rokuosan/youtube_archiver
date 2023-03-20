63ea083ed7ed317b76498dc5c3a780a012a5da78
rokuosan <85651386+rokuosan@users.noreply.github.com>
2023-03-20 22:00:10 +0900

    オプションを変更してDLできるようにした

--- youtube_archiver/youtube_archiver_app.py
+++ youtube_archiver/youtube_archiver_app.py
@@ -1,3 +1,5 @@
+import re
+
 import flet as ft
 import urllib.request
 
@@ -5,6 +7,7 @@ from glob import glob
 from yt_dlp import YoutubeDL
 from time import sleep
 
+from youtube_archiver.component.SettingDialog import SettingDialog, get_prop
 from youtube_archiver.component.Video import Video
 from youtube_archiver.util.Logger import Logger
 
@@ -16,7 +19,7 @@ download_list = {}
 def progress_hook(d):
     info = d['info_dict']
     fid = info['id']
-    filename = info['title']
+    filename = re.sub(r'[\\/:*?"<>|]+', '-', str(info['title']))
     video: Video
 
     if fid in download_list:
@@ -46,10 +49,6 @@ def progress_hook(d):
 
 
 class YouTubeArchiver(ft.UserControl):
-    YTDL_OPT = {
-        'progress_hooks': [progress_hook],
-        'logger': Logger()
-    }
 
     def __init__(self):
         super().__init__()
@@ -59,12 +58,18 @@ class YouTubeArchiver(ft.UserControl):
         self.download_url = None
         self.download_btn = None
 
+        self.option_btn = None
+
+        self.dialog = SettingDialog()
+
     def build(self):
         self.video_list = ft.ListView(expand=1, spacing=0, padding=0, auto_scroll=True)
 
         self.download_url: ft.TextField = ft.TextField(hint_text='YouTube URL',
                                                        expand=True, on_submit=self.download_clicked)
         self.download_btn: ft.IconButton = ft.IconButton(ft.icons.DOWNLOAD, on_click=self.download_clicked)
+
+        self.option_btn = ft.IconButton(ft.icons.SETTINGS, on_click=self.option_clicked)
 
         return ft.Column(
             width=400,
@@ -73,6 +78,7 @@ class YouTubeArchiver(ft.UserControl):
                 ft.Row(
                     width=390,
                     controls=[
+                        self.option_btn,
                         self.download_url,
                         self.download_btn
                     ]
@@ -82,6 +88,26 @@ class YouTubeArchiver(ft.UserControl):
         )
 
     def download_clicked(self, e):
+
+        if get_prop('audio_only'):
+            opt = {
+                'progress_hooks': [progress_hook],
+                'logger': Logger(),
+                'format': 'm4a/bestaudio/best',
+                'outtmpl': get_prop('output_template'),
+                'postprocessors': [{  # Extract audio using ffmpeg
+                    'key': 'FFmpegExtractAudio',
+                    'preferredcodec': 'm4a',
+                }]
+            }
+        else:
+            opt = {
+                'progress_hooks': [progress_hook],
+                'logger': Logger(),
+                'format': get_prop('format'),
+                'outtmpl': get_prop('output_template')
+            }
+
         # Get URL and clear text field
         url: str = self.download_url.value
         self.download_url.value = ''
@@ -93,12 +119,13 @@ class YouTubeArchiver(ft.UserControl):
             return
 
         # Download
-        with YoutubeDL(self.YTDL_OPT) as yt:
+        with YoutubeDL(opt) as yt:
             info = yt.extract_info(url, download=False)
             thumb_url: str = yt.sanitize_info(info)['thumbnails'].pop()['url']
+            title = re.sub(r'[\\/:*?"<>|]+', '-', str(info['title']))
 
             # Check exists
-            files = glob(f"{info['title']}*")
+            files = glob(f"{title}*")
             if len(files) > 0:
                 self.page.snack_bar = ft.SnackBar(ft.Text('The video has already downloaded.'))
                 self.page.snack_bar.open = True
@@ -108,16 +135,16 @@ class YouTubeArchiver(ft.UserControl):
                 return
 
             # Get thumbnails
-            urllib.request.urlretrieve(thumb_url, f"{info['title']}.png")
+            urllib.request.urlretrieve(thumb_url, f"{title}.png")
 
             # Add Video
-            video = Video(info["title"], f"{info['title']}.png")
+            video = Video(title, f"{title}.png")
             self.video_list.controls.append(video)
             self.update()
 
             # Download
-            pending_list[info['title']] = video
-            print(f'{info["title"]}')
+            pending_list[title] = video
+            print(f'{title}')
             yt.download(url)
 
     def url_validation(self, url: str) -> bool:
@@ -133,3 +160,8 @@ class YouTubeArchiver(ft.UserControl):
             return False
 
         return True
+
+    def option_clicked(self, e):
+        self.page.dialog = self.dialog
+        self.page.update()
+        self.dialog.show()
